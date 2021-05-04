9312d7c2adca31fbcc64bdca1205bd1241a87548
Vango199 <62698943+Vango199@users.noreply.github.com>
2021-05-04 08:00:33 +0200

    Update Dame.java

--- src/model/Dame.java
+++ src/model/Dame.java
@@ -4,7 +4,7 @@ public class Dame extends AbstractPiece {
 	
 	public Dame(Couleur _couleur, Coord _coordonnee ) {
 		super(_couleur, _coordonnee);
-		this.Dame = "Fou";
+		this.name = "Dame";
 		// TODO Auto-generated constructor stub
 	}
 	public boolean isMoveOk(int xFinal, int yFinal) {
